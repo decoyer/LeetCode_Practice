860444ce285413adb63ba8684d55e32f14fa9b69
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 16:22:18 +0900

    Update 1051-height-checker.java

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,14 +1,16 @@
 class Solution {
     public int heightChecker(int[] heights) {
         int cnt = 0;
+        // 비교를 위한 배열 생성
         int arr[] = heights.clone();
-        
+
+        // 배열 정렬
         Arrays.sort(arr);
-        
+
+        // 배열 비교
         for (int i = 0; i < heights.length; i++) {
-            if (arr[i] != heights[i]) {
+            if (arr[i] != heights[i])
                 cnt++;
-            }
         }
         
         return cnt;
