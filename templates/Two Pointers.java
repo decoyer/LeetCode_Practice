ee9bf340f5124f29763c6db90de919c927e60511
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 13:40:53 +0900

    Create Two Pointers.java

---
+++ templates/Two Pointers.java
@@ -0,0 +1,43 @@
+// one input, opposite ends
+public int fn(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+    int ans = 0;
+
+    while (left < right) {
+        // do some logic here with left and right
+        if (CONDITION) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return ans;
+}
+
+// two inputs, exhaust both
+public int fn(int[] arr1, int[] arr2) {
+    int i = 0, j = 0, ans = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        // do some logic here
+        if (CONDITION) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        // do logic
+        i++;
+    }
+
+    while (j < arr2.length) {
+        // do logic
+        j++;
+    }
+
+    return ans;
+}
