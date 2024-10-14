81ec3ae15f04b4e85f9ef93fa47642c86960ae06
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 17:39:20 +0900

    Update 0977-squares-of-a-sorted-array.java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        // 각 요소 제곱 후 저장
         for (int i = 0; i < nums.length; i++)
             nums[i] *= nums[i];
-        
+
+        // 배열 정렬
         Arrays.sort(nums);
 
         return nums;
