60830be0a5f24bb102ef5182a75ab7624ab164e0
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 17:41:00 +0900

    Time: 1 ms (24.34%), Space: 49.7 MB (9.85%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         for (int i = 0; i < s.length / 2; i++) {
-            char tmp = '';
+            char tmp = ' ';
             
             tmp = s[i];
             s[i] = s[s.length - 1 - i];
