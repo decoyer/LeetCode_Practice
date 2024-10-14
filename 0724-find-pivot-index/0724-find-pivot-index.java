01f9372deb55f40706a0870496e3e0a64b750b46
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 10:43:43 +0900

    Update 0724-find-pivot-index.java

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,9 +3,11 @@ class Solution {
         int sum = 0;
         int left = 0;
 
+        // nums 배열 요소들의 합
         for (int num : nums)
             sum += num;
-        
+
+        // i를 기준으로 왼쪽 요소들의 합을 구하여 전체 합에서 뺀 값과 비교 (왼쪽 합 == 오른쪽 합)
         for (int i = 0; i < nums.length; i++) {
             if (left == sum - left - nums[i])
                 return i;
