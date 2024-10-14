4d02b80c6185726cfc017c277eef7e6badf6b0b0
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 13:34:56 +0900

    Create Linked List.java

---
+++ templates/Linked List.java
@@ -0,0 +1,14 @@
+// Fast and Slow Pointer
+public int fn(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    int ans = 0;
+
+    while (fast != null && fast.next != null) {
+        // do logic
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return ans;
+}
