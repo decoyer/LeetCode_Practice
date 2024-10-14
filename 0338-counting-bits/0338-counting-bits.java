00c209e98e0ffe809262c953749ebab3ede85d12
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 11:57:33 +0900

    Update 0338-counting-bits.java

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
         int result[] = new int[n + 1];
+        // 첫번째 요소 초기화
         result[0] = 0;
         
         for (int i = 1; i <= n; i++) {
-
             int num = i;
             int cnt = 0;
+
+            // 비트 연산자 활용하여 재귀 순환
             while (num != 0) {
                 num &= num - 1;
                 cnt++;
