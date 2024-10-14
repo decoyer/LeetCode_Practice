0cc6b3918b4c67d06a24fa5fab196c90cf95d8c1
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 12:23:55 +0900

    Update 0941-valid-mountain-array.java

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -2,24 +2,28 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
         int max = 0;
         int idx = 0;
-        
+
+        // 배열 안의 최대값과 인덱스 저장
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] > max) {
                 max = arr[i];
                 idx = i;
             }
         }
-        
-        if (arr.length < 3 || idx == 0 || idx == arr.length - 1) {
+
+        // 제약조건 확인
+        if (arr.length < 3 || idx == 0 || idx == arr.length - 1)
             return false;
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 오르막길 확인
+            if (i < idx && arr[i] >= arr[i+1])
+                return false;
+            // 내리막길 확인
+            else if (i >= idx && arr[i] <= arr[i+1])
+                return false;
         }
         
-        for(int i = 0; i < arr.length - 1; i++) {
-            if(i < idx && arr[i] >= arr[i+1])
-                return false;
-            else if (arr[i] <= arr[i+1])
-                return false;
-        }
         return true;
     }
 }
