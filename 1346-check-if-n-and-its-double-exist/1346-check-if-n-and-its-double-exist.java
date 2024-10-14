8a18e3a337f8b5262a8065e767ce88bf12e4a819
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 12:16:49 +0900

    Time: 2 ms (94.68%), Space: 42.5 MB (86.29%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i <= arr.length-1; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             int j = 0;
-            while (j <= arr.length-1) {
-                if (arr[i] * 2 == arr[j] && i != j) {
+            while (j <= arr.length - 1) {
+                if (arr[i] * 2 == arr[j] && i != j)
                     return true;
-                }
                 j++;
             }
         }
