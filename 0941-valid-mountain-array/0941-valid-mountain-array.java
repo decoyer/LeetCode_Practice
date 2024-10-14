e930219287af006c35f7160fc23b850444b2209d
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 12:21:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
         int max = 0;
-        int index = 0;
+        int idx = 0;
         
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if(arr[i] > max) {
                 max = arr[i];
-                index = i;
+                idx = i;
             }
         }
         
-        if(arr.length < 3 || index == 0 || index == arr.length - 1) {
+        if (arr.length < 3 || idx == 0 || idx == arr.length - 1) {
             return false;
         }
         
         for(int i = 0; i < arr.length - 1; i++) {
-            if(i < index) {
-                if(arr[i] >= arr[i+1]) return false;
-            }
-            else {
-                if(arr[i] <= arr[i+1]) return false;
-            }
+            if(i < idx && arr[i] >= arr[i+1])
+                return false;
+            else if (arr[i] <= arr[i+1])
+                return false;
         }
         return true;
     }
