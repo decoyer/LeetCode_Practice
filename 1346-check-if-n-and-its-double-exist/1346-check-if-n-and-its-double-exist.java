6a1716f2bffc4249861a2226e15729b511e5e504
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 12:17:44 +0900

    Update 1346-check-if-n-and-its-double-exist.java

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        // 배열 순회
         for (int i = 0; i <= arr.length - 1; i++) {
             int j = 0;
+            // 서로 다른 2개의 요소 중 조건에 맞는 경우 확인
             while (j <= arr.length - 1) {
                 if (arr[i] * 2 == arr[j] && i != j)
                     return true;
