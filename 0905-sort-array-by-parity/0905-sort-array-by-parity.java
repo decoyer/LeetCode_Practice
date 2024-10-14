28ec4be388e79129b2f7d0035e1c4e76d9af0139
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 16:17:33 +0900

    Update 0905-sort-array-by-parity.java

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -2,14 +2,18 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        
+
+        // 2개의 포인터
         while (left < right) {
+            // 배열의 왼쪽에는 짝수 요소
             if (nums[left] % 2 == 0)
                 left++;
 
+            // 배열의 오른쪽에는 홀수 요소
             else if (nums[right] % 2 == 1)
                 right--;
-            
+
+            // 짝수 요소와 홀수 요소 자리 바꾸기
             else {
                 int tmp = nums[left];
                 nums[left] = nums[right];
