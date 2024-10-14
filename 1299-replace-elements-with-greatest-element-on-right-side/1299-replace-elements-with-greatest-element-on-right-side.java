a4a36f37cb35dd68b7059db32c6a07f8f2cdccb1
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 13:55:49 +0900

    Time: 3 ms (60.04%), Space: 46.9 MB (6.16%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
         if (arr == null || arr.length == 1)
-        {
             return new int[]{-1};
-        }
         
-        for (int i = arr.length-1; i >= 1; i--) {
-            if (arr[i] > arr[i-1]) {
-                arr[i-1] = arr[i];
-            }
+        for (int i = arr.length - 1; i >= 1; i--) {
+            if (arr[i] > arr[i - 1])
+                arr[i - 1] = arr[i];
+
             if (i == 1) {
                 for (int j = 1; j < arr.length; j++) {
-                    arr[j-1] = arr[j];
-                    if (j == arr.length-1) {
+                    arr[j - 1] = arr[j];
+                    if (j == arr.length - 1) {
                         arr[j] = -1;
                     }
                 }
