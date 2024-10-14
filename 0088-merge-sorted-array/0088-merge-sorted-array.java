271bd301ba4652cd6ee33071ec87f618f6b42622
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 12:12:07 +0900

    Time: 1 ms (32.72%), Space: 42.2 MB (44.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m;
-        for (int j = 0; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
+        int idx = m;
+
+        // 배열 병합
+        for (int i = 0; i < n; i++) {
+            nums1[idx++] = nums2[i];
         }
+        // 배열 정렬
         Arrays.sort(nums1);
     }
 }
