48a8c6530655e5fa63a45d089da502a55a9127f5
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 13:32:50 +0900

    Update Two Pointers.java

--- templates/Two Pointers.java
+++ templates/Two Pointers.java
@@ -1,3 +1,4 @@
+// one input, opposite ends
 public int fn(int[] arr) {
     int left = 0;
     int right = arr.length - 1;
@@ -10,6 +11,32 @@ public int fn(int[] arr) {
         } else {
             right--;
         }
+    }
+
+    return ans;
+}
+
+// two inputs, exhaust both
+public int fn(int[] arr1, int[] arr2) {
+    int i = 0, j = 0, ans = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        // do some logic here
+        if (CONDITION) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        // do logic
+        i++;
+    }
+
+    while (j < arr2.length) {
+        // do logic
+        j++;
     }
 
     return ans;
