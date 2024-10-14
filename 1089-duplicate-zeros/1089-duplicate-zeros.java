7d8bf38d2438ef96191c9874aebeaae562314146
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 11:54:36 +0900

    Update 1089-duplicate-zeros.java

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,6 +1,7 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            // 배열에서 0 나오면 0부터 오른쪽 요소 다시 저장
             if (arr[i] == 0) {
                 for (int j = arr.length - 1; j > i; j--) {
                     arr[j] = arr[j - 1];
