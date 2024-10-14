6e82f084e80962b4b9867fefa134ecb81f7beb18
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-14 10:44:53 +0900

    Update 0747-largest-number-at-least-twice-of-others.java

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -2,17 +2,20 @@ class Solution {
     public int dominantIndex(int[] nums) {
         int max = 0;
         int idx = 0;
-        
+
+        // 배열 안의 최대값과 인덱스 저장
         for (int i = 0; i < nums.length; i++) {
             if (max < nums[i]) {
                 max = nums[i];
                 idx = i;
             }
         }
-        
+
         for (int i = 0; i < nums.length; i++) {
+            // 최대값 인덱스는 스킵
             if (i == idx)
                 continue;
+            // 최대값이 배열 요소의 2배보다 작으면 -1 반환
             if (max < nums[i] * 2)
                 return -1;
         }
