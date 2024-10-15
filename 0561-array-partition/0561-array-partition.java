2c1b1da4cb79a3d553bb26f32e62e01e77e45635
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-15 11:32:31 +0900

    Update 0561-array-partition.java

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -2,8 +2,10 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         int result = 0;
 
+        // 배열 정렬
         Arrays.sort(nums);
-        
+
+        // [a, b] (a <= b)의 구조에서 a값끼리 더함
         for (int i = 0; i < nums.length; i++)
             result += nums[i++];
         
