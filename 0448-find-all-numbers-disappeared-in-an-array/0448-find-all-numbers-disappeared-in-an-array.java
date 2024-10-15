9dda17c6d997ea18cde0b4d2a09854baca944cd4
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-15 11:04:01 +0900

    Time: 26 ms (5.26%), Space: 54 MB (62.78%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -5,11 +5,8 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i+1) {
-                if (Arrays.binarySearch(nums, i+1) < 0) {
-                    result.add(i+1);
-                }
-            }
+            if (Arrays.binarySearch(nums, i + 1) < 0)
+                result.add(i + 1);
         }
         
         return result;
