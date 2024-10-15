e1a74ad41d1844af8b808f759f371705872f4521
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-15 12:02:14 +0900

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,6 +6,7 @@ class Solution {
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
 
+            // 다른 단어 발견 시 저장된 문자열 반환
             for (String str : strs) {
                 if (str.length() <= i || c != str.charAt(i))
                     return sb.toString();
