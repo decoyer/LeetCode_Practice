eb5dafcfbef73b67fa41aac4845345d11cc2a7d7
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-16 10:53:18 +0900

    Update 0119-pascals-triangle-ii.java

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -2,12 +2,14 @@ class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new ArrayList<>();
         long num = 1;
-        
+
+        // 이항정리 공식을 활용하여 계산 후 값 저장
         for (int i = 0; i < rowIndex; i++) {
             list.add((int)num);
             num = num * (rowIndex - i) / (i + 1);
         }
 
+        // 삼각형의 마지막 항은 1로 저장
         list.add(1);
 
         return list;
