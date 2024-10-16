8b87b58a9e22ea31a5de830b62ec3e68566f90c6
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-16 09:30:00 +0900

    Update 0485-max-consecutive-ones.java

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,13 +1,15 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int cnt = 0, result = 0;
-        
+
+        // 배열 요소의 값이 0이면 초기화, 0이 아니면 카운트
         for (int num : nums) {
             if (num == 0)
                 cnt = 0;
             else
                 cnt++;
-            
+
+            // 최대 카운트(연속) 값을 저장
             result = (result < cnt) ? cnt : result;
         }
         
