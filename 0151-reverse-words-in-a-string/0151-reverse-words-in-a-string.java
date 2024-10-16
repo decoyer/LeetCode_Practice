4cf6c1d8c997b9a163e872883c3253e7907c2f23
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-16 11:10:37 +0900

    Update 0151-reverse-words-in-a-string.java

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,11 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.replaceAll(" +", " ").split(" ");
         StringBuilder sb = new StringBuilder();
+        // 2칸 이상 공백을 1칸으로 바꾸고, 단어만 배열에 저장
+        String[] words = s.replaceAll(" +", " ").split(" ");
 
+        // 배열에 저장된 단어를 역순으로 문자열에 저장
         for (int i = words.length - 1; i >= 0; i--)
             sb.append(words[i] + " ");
-        
+
+        // 저장된 문자열의 앞 뒤 공백 제거 후 반환
         return sb.toString().trim();
     }
 }
