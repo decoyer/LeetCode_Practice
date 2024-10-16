fcc9b8b816d62132e787e3ce1e4331fc066218ab
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-16 09:14:59 +0900

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int index = 0;
-        
+
+        // val 값과 다를 때 카운트 및 저장
         for (int num : nums) {
             if (num != val)
                 nums[index++] = num;
