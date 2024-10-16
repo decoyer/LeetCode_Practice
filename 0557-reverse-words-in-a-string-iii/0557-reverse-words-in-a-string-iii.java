376baed72e1bf34fd617d8df493d0bc4027dcac2
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-16 11:22:31 +0900

    Update 0557-reverse-words-in-a-string-iii.java

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,13 +1,16 @@
 class Solution {
     public String reverseWords(String s) {
         StringBuilder result = new StringBuilder();
+        // 공백을 제외한 단어만 배열에 저장
         String[] arr = s.split(" ");
-        
+
+        // 배열에 저장된 단어 조합을 역순으로로 문자열에 저장
         for (String words : arr) {
             StringBuilder sb = new StringBuilder(words);
             result.append(sb.reverse() + " ");
         }
-        
+
+        // 저장된 문자열의 앞 뒤 공백 제거 후 반환
         return result.toString().trim();
     }
 }
