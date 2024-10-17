1aa15d6cdbba0f6e0fd743c16b46092b6452a650
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-17 11:04:29 +0900

    Time: 0 ms (100%), Space: 44.2 MB (77.77%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,14 +1,3 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
@@ -18,9 +7,8 @@ public class Solution {
             fast = fast.next.next;
             slow = slow.next;
 
-            if (fast == slow) {
+            if (fast == slow)
                 return true;
-            }
         }
 
         return false;
