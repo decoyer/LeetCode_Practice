5f410ea1e026a748318f5f7cf85b89c83d970d98
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-17 10:52:01 +0900

    Update 0283-move-zeroes.java

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,8 +2,11 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int idx = 0;
 
+        // 2개의 포인터로 배열 순회
         for (int i = 0; i < nums.length; i++) {
+            // 값이 0인 요소 탐색(1번 포인터)
             if (nums[i] != 0) {
+                // 1개의 포인터만 값이 0이고 서로 다른 자리에 있을 때 자리 바꿈
                 if (nums[idx] == 0 && i != idx) {
                     int tmp = nums[i];
                     nums[i] = nums[idx];
@@ -11,6 +14,7 @@ class Solution {
                 }
             }
 
+            // 값이 0인 요소 탐색(2번 포인터)
             if (nums[idx] != 0)
                 idx++;
         }
