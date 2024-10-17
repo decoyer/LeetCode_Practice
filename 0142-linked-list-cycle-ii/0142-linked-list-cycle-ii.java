639c189c6115a53d3e81d7256eb53e2dac7db40e
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-17 11:18:30 +0900

    Update 0142-linked-list-cycle-ii.java

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,17 +1,21 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        // 입력 노드를 서로 다른 2개의 노드로 저장
         ListNode fast = head;
         ListNode slow = head;
 
+        // 1칸씩 이동하는 노드가 2칸씩 이동하는 노드와 만나는지 확인(싸이클이 존재)
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            
+
+            // 싸이클이 존재하면 
             if (fast == slow) {
                 slow = head;
+                
                 while (slow != fast) {
-                slow = slow.next;
-                fast = fast.next;
+                    slow = slow.next;
+                    fast = fast.next;
                 }
                 
                 return slow;
