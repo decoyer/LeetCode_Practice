06bc913169333c4810ddc7fcf70d3d210229107e
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-17 10:05:09 +0900

    Update 0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,6 +2,7 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int index = 1;
 
+        // 배열 순회 및 요소 비교하여 중복되지 않은 값만 저장
         for (int i = 0; i < nums.length - 1; i++)
             if (nums[i] != nums[i + 1])
                 nums[index++] = nums[i + 1];
