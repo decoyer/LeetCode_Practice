c48481bfe142b3411e9bf169f4144e88ffe5ded5
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 11:55:03 +0900

    Update 0019-remove-nth-node-from-end-of-list.java

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,20 +1,25 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 더미 노드 생성 및 2개의 포인터 연결
         ListNode tmp = new ListNode(0);
         ListNode start = tmp;
         ListNode end = tmp;
         tmp.next = head;
-        
+
+        // 삭제하려는 노드 1칸 뒤에 포인터1 위치
         for (int i = 0; i <= n; i++)
             end = end.next;
-        
+
+        // 포인터1 노드 끝까지 이동할 때까지 포인터2 이동
         while (end != null) {
             start = start.next;
             end = end.next;
         }
-        
+
+        // 삭제하려는 노드 1칸 뒤에 포인터2 위치
         start.next = start.next.next;
-        
+
+        // 삭제하려는 노드 스킵하여 반환
         return tmp.next;
     }
 }
