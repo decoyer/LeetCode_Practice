9b7d11a892fdaf39a481eef00125d7ca531c85eb
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 11:07:08 +0900

    Update 0160-intersection-of-two-linked-lists.java

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -2,12 +2,15 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
-        
+
+        // 2개의 노드를 1칸씩 이동시키면서 같은 노드인지 확인
         while (a != b) {
+            // 노드 끝까지 가면 다른 노드에서 다시 출발
             if (a == null)
                 a = headB;
             else
                 a = a.next;
+            // 노드 끝까지 가면 다른 노드에서 다시 출발
             if (b == null)
                 b = headA;
             else
