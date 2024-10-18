8152705b88ef7ff10a0a65ed05f283085864563f
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 11:05:54 +0900

    Time: 1 ms (99.87%), Space: 48.2 MB (79.21%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,32 +1,17 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
         
         while (a != b) {
-            if (a == null) {
+            if (a == null)
                 a = headB;
-            }
-            else {
+            else
                 a = a.next;
-            }
-            if (b == null) {
+            if (b == null)
                 b = headA;
-            }
-            else {
+            else
                 b = b.next;
-            }
         }
 
         return a;
