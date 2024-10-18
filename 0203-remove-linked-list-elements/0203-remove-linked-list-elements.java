cb62189232970e6304f4d11986de6e32b486f40c
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 12:20:17 +0900

    Update 0203-remove-linked-list-elements.java

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -1,10 +1,13 @@
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        // 노드 값이 있을 때만 반환
         if (head == null)
             return null;
 
+        // 재귀형 함수로 작성
         head.next = removeElements(head.next, val);
 
+        // 노드 값이 val 값과 같으면 스킵하여 반환
         if (head.val == val)
             return head.next;
         else
