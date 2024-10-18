d5be62fc179d2df4113dfc891f5ea62fb4a505ee
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-18 12:08:49 +0900

    Update 0206-reverse-linked-list.java

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,7 +1,8 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode result = null;
-        
+
+        // 더미 노드 생성 및 노드 값 저장
         while (head != null) {
             ListNode tmp = head;
             
