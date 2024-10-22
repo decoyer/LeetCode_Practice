857e43eaba72ade8347fd1a6bd22020d1abce44e
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-22 15:00:17 +0900

    Update 0328-odd-even-linked-list.java

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,13 +1,16 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        // 예외 처리
         if (head == null) {
             return head;
         }
-        
+
+        // 홀수 번째 노드, 짝수 번째 노드, 더미 노드 생성
         ListNode odd = head;
         ListNode even = head.next;
         ListNode tmp = even;
-        
+
+        // 홀수 노드, 짝수 노드 순회
         while (odd.next != null && even.next != null) {
             odd.next = odd.next.next;
             even.next = even.next.next;
@@ -15,6 +18,7 @@ class Solution {
             even = even.next;
         }
         
+        // 홀수 노드 끝에 짝수 노드 연결
         odd.next = tmp;
         
         return head;
