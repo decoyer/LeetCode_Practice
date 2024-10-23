6e94752775d0c1c27ffcee615ab6f8ad643cbb5e
decoyer <87348583+decoyer@users.noreply.github.com>
2024-10-23 10:12:07 +0900

    Update 1492-the-kth-factor-of-n.java

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -2,10 +2,12 @@ class Solution {
     public int kthFactor(int n, int k) {
         List<Integer> list = new ArrayList<>();
 
+        // 리스트에 요소 저장
         for (int i = 1; i <= n; i++)
             if (n % i == 0)
                 list.add(i);
 
+        // 약수의 개수가 부족하면 -1 반환
         if (list.size() < k)
             return -1;
 
